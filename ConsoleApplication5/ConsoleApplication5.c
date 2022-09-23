0a4029ff7c00d6e17bb4c56fd1069fdab4ef842a
Sm0keF1nger <kkaptelov@gmail.com>
2022-09-23 15:32:28 +0300

    first commit

---
+++ ConsoleApplication5/ConsoleApplication5.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+	int a, b;
+	double x;
+	printf("a = ");
+	scanf_s("%d", &a);
+
+	printf("b = ");
+	scanf_s("%d", &b);
+
+	x = (2 * cos(a) - pow(b, 2)) / (3 * (pow(a, 2) - 3 * b));
+
+	printf("x = 2cos(a) - b^2 / 3(a^2 - 3b)\n x = %.2lf\n", x);
+}
